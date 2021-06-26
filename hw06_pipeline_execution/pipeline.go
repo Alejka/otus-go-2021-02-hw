b5dc42e3bade23fc57237e9b84f9d5cae3cfba39
Oleksii Samokhvalov <samohvalov.aleksey@gmail.com>
2021-06-26 21:05:50 +0300

    HW6 is completed

--- hw06_pipeline_execution/pipeline.go
+++ hw06_pipeline_execution/pipeline.go
@@ -8,7 +8,40 @@ type (
 
 type Stage func(in In) (out Out)
 
-func ExecutePipeline(in In, done In, stages ...Stage) Out {
-	// Place your code here.
-	return nil
+func ExecutePipeline(in In, terminate In, stages ...Stage) Out {
+	if in == nil {
+		in := make(chan interface{})
+		close(in)
+		return in
+	}
+
+	for _, stage := range stages {
+		ch := make(Bi)
+		go func(ch Bi, o Out) {
+			defer close(ch)
+			for {
+				select { // increase priority of terminate chan
+				case <-terminate:
+					return
+				default:
+				}
+
+				select {
+				case <-terminate:
+					return
+				case v, ok := <-o:
+					if !ok {
+						return
+					}
+					select {
+					case <-terminate: // chan "ch" can be blocked at this moment
+						return
+					case ch <- v:
+					}
+				}
+			}
+		}(ch, in)
+		in = stage(ch)
+	}
+	return in
 }
